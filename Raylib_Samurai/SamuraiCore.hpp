819c8981a4e659725b418d8f36c987f0f047aa3c
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-07-09 17:15:07 -0600

    feat: Core features

---
+++ Raylib_Samurai/SamuraiCore.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <raylib.h>
+
+namespace sam_core
+{
+
+	//Set Windows config: Takes the physical monitor values and set the max window size, VSYNC, Size, etc...
+	bool SetWindowConfig()
+	{
+		InitWindow(300, 300, "Samurai - Verificador");
+
+		if (!IsWindowReady())
+			return false;
+		else
+		{
+			int* _currentMonitor = new int(GetCurrentMonitor());
+
+			int* _physicalMonitorHeight = new int(GetMonitorPhysicalHeight(*_currentMonitor));
+			int* _physicalMonitorWidth = new int(GetMonitorPhysicalWidth(*_currentMonitor));
+
+			SetWindowMaxSize(*_physicalMonitorWidth, *_physicalMonitorHeight);
+			SetWindowSize(*_physicalMonitorWidth, *_physicalMonitorHeight);
+
+			SetTargetFPS(GetMonitorRefreshRate(*_currentMonitor));
+			SetWindowState(FLAG_VSYNC_HINT);
+			SetWindowTitle("Samurai");
+
+			delete _currentMonitor;
+			_currentMonitor = nullptr;
+
+			delete _physicalMonitorHeight;
+			_physicalMonitorHeight = nullptr;
+
+			delete _physicalMonitorWidth;
+			_physicalMonitorWidth = nullptr;
+
+			return true;
+		}
+	}
+	
+	// Close all devices so the window can close properly
+	void CloseProperly()
+	{
+		CloseAudioDevice();
+		CloseWindow();
+	}
+}
--- Raylib_Samurai/main.cpp
+++ Raylib_Samurai/main.cpp
@@ -2,20 +2,9 @@
 
 int main()
 { 
-	InitWindow(800, 600, "Raylib Samurai Game");
-	if(!IsWindowReady())
-		return -1;
-
-
-	while(!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("Welcome to Raylib Samurai Game!", 190, 280, 20, LIGHTGRAY);
-		DrawText("Press ESC to exit", 300, 320, 20, LIGHTGRAY);
-		EndDrawing();
-	}
 	
-	CloseWindow();
+
+
+	
 	return 0;
 }
